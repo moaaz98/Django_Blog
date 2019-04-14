1d05380b377a900e295cc02b2a7636f023df26be
Moaaz Ahmed <moaaz98a@gmail.com>
2019-04-14 14:50:58 +0400

    Second Commit

---
+++ articles/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Article
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'content', 'pic', ]
+
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+        widgets = {
+            "password": forms.PasswordInput(),
+        }
+
+class SigninForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True)
+    pic = models.ImageField(null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "created on %s - %s"%(self.created, self.title)
